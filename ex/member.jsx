66b2184c0cd9636186cfaa97ce34cbc99da95bfe
Clebio <clebiovieira@gmail.com>
2017-03-26 19:06:18 -0300

    Aula 25

--- ex/member.jsx
+++ ex/member.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default props => (
     <div>
-    {props.name}<strong>{props.lastName}</strong>
+    {props.name} <strong>{props.lastName}</strong>
     </div>    
 )
